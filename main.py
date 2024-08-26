b09dfba2cd21a8ca2f88d27bfca6a2758137148d
CaffiendDevKit <github.scoop462@passfwd.com>
2024-08-26 22:34:03 +0800

    Made the player rotate on key press

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(TARGET_FPS) / 1000 # 60 FPS
 
